c51eaf76808b78c6c3374e0b50c3a772d3fef52f
Mohamed Ibrahim <mohamed.ibrahim1@bp.com>
2022-11-23 15:49:49 +0200

    fix: fix specified python version and auth issue

--- vending_machine_api/products/views.py
+++ vending_machine_api/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -27,7 +27,7 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, ProductAccessPolicy]
+    permission_classes = [IsAuthenticated, ProductAccessPolicy]
 
     @action(detail=False, methods=["post"], serializer_class=BuyProductSerializer)
     def buy(self, request, pk=None):
